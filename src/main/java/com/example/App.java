462d6059c4eb26af97243216761b9b0570c8bbc7
Tanmay Gupta <tanmaygupta2610@gmail.com>
2025-05-01 18:23:31 +0000

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,12 +3,21 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        WebDriver driver=new ChromeDriver();
+public class App {
+    public static void main(String[] args) {
+        // Setup ChromeOptions
+        ChromeOptions options = new ChromeOptions();
+        // Avoid user-data-dir issues
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*"); // often required for newer ChromeDriver
+        // Use headless if you want no UI: options.addArguments("--headless=new");
+
+        // Start ChromeDriver with options
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
